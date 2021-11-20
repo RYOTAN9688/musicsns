c568e007277d8cd31af46fbdd153b22eaa884337
RYOTAN9688 <ryota8813@gmail.com>
2021-11-20 09:49:30 +0900

    form 作成

---
+++ src/components/atoms/InputText/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px 15px;
+  font-size: 16px;
+  border-radius: 4px;
+  border: 1px solid #ddd;
+  box-sizing: border-box;
+  :focus {
+    border-color: rgba(100, 100, 255, 0.5);
+  }
+  ::placeholder {
+    color: #ddd;
+  }
+`;
+
+type Props = {
+  onClick?: (event: React.MouseEvent<HTMLInputElement>) => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+};
+
+const InputText: React.FC<Props> = ({ ...props }) => {
+  return <Input type='text' {...props} />;
+};
+
+export default InputText;
